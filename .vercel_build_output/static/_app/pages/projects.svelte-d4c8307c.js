e95a440a757c74293f8adf1eb73b1f396ace491e
Koen Raijer <koenraijer@Koens-MacBook-Air.local>
2021-12-01 15:02:33 +0100

    update

---
+++ .vercel_build_output/static/_app/pages/projects.svelte-d4c8307c.js
@@ -0,0 +1,1 @@
+import{S as c,i,s as l,e as d,t as h,c as f,a as m,g as p,d as o,f as u,H as _,J as n}from"../chunks/vendor-7fa6c8a1.js";function x(r){let e,s;return{c(){e=d("h1"),s=h("Projects")},l(t){e=f(t,"H1",{});var a=m(e);s=p(a,"Projects"),a.forEach(o)},m(t,a){u(t,e,a),_(e,s)},p:n,i:n,o:n,d(t){t&&o(e)}}}class v extends c{constructor(e){super();i(this,e,null,x,l,{})}}export{v as default};
--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 	preprocess: [SveltePreprocess(), mdsvex(mdsvexConfig)],
 
 	kit: {
-		adapter: vercel()
+		adapter: vercel(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
