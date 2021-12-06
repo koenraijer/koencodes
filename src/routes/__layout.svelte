a593806e88bd8ac29d53a2539a6a864888ce67da
Koen Raijer <koenraijer@Koens-MacBook-Air.local>
2021-12-06 18:53:10 +0100

    name change

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -9,7 +9,7 @@ import { page } from "$app/stores";
 
 
 <Nav />
-<h1 class="sitetitle"><a on:click={open = false} href="/">koen.wtf</a></h1>
+<h1 class="sitetitle"><a on:click={open = false} href="/">koen.codes</a></h1>
 <div class="container">
   <slot />
 </div>
